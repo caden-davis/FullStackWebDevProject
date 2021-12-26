a429085b40cfec533b6106fc2394518c474e4239
Caden Davis <cadend9@gmail.com>
2021-12-25 18:42:59 -0800

    Database API -- MySQL implementation, still kept in generics

--- DatabaseAPI/controllers/apiControllers.js
+++ DatabaseAPI/controllers/apiControllers.js
@@ -1,26 +1,46 @@
 'use strict';
 
 // Define database model as variable here
+var conn = require('apiModels');
 
 // Define controller functions here
 exports.getOne = function(req, res) {
-
+    conn.query(SQLgetOne, (err, queryRes) => {
+        if (err) throw err;
+        res.json(queryRes);
+    })
 };
 exports.getAll = function(req, res) {
-
+    conn.query(SQLgetAll, (err, queryRes) => {
+        if (err) throw err;
+        res.json(queryRes);
+    })
 };
 exports.getRange = function(req, res) {
     // gets range from req.params.ID1 to req.params.ID2
+    conn.query(SQLgetRange, (err, queryRes) => {
+        if (err) throw err;
+        res.json(queryRes);
+    })
 };
 exports.delete = function(req, res) {
     // needs req.params.ID
-
+    conn.query(SQLdelete, (err, queryRes) => {
+        if (err) throw err;
+        res.json(queryRes);
+    })
 };
 exports.post = function(req, res) {
     // needs both postOne and postAll functionality depending on req
-
+    conn.query(SQLpost, (err, queryRes) => {
+        if (err) throw err;
+        res.json(queryRes);
+    })
 };
 exports.put = function(req, res) {
     // aka "update" function
-
+    conn.query(SQLput, (err, queryRes) => {
+        if (err) throw err;
+        res.json(queryRes);
+    })
 };
--- DatabaseAPI/models/apiModels.js
+++ DatabaseAPI/models/apiModels.js
@@ -6,5 +6,35 @@ port = process.env.PORT || 3001;
 'use strict';
 
 // Define database schema
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+    host: "localhost",
+    user: "cadendavis",
+    password: "Konakona123!"
+});
+
+/*
+// FIRST TIME SETUP TO CREATE DATABASE
+conn.connect((err) => {
+    if (err) throw err;
+    conn.query("CREATE DATABASE db", function(err, res) {
+        if (err) throw err;
+    })
+})
+
+// FIRST TIME SETUP TO CREATE TABLES
+conn.connect((err) => {
+    if (err) throw err;
+    conn.query("CREATE TABLE t1 (fieldname type, fieldname type)", function(err, res) {
+        if (err) throw err;
+    })
+})
+*/
+
+conn.connect((error) => {
+    if (error) throw error;
+    console.log("Connected to MySQL Database");
+});
 
 // Expose the database model (module.exports = db.model)
+module.exports = conn;
