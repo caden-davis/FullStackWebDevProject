746ef37189ffe8ab896867e2868a3f9a0b9214ec
Caden Davis <cadend9@gmail.com>
2021-12-30 12:37:54 -0800

    Config.properties file, support for batch inference

--- TorchServeAPI/ts_model.py
+++ TorchServeAPI/ts_model.py
@@ -9,19 +9,6 @@ class Net(nn.Module):
     """
     def __init__(self):
         super(Net, self).__init__()
-        """
-        Input Features:
-        1. 
-        2. 
-        3. 
-        4.
-
-        Output:
-
-        These requirements lend themselves to the following pseudo-structure:
-        1. 
-        2. 
-        """
         self.nn = nn.Sequential(
 
         )
